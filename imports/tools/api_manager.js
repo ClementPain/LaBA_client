af8d9ed326c515a8e7c0c692f6db63922ecf2bf7
clement <clementpain@outlook.com>
2021-03-01 11:20:25 +0100

    change url production / dev

--- imports/tools/api_manager.js
+++ imports/tools/api_manager.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-const url = process.env.API_URL
+let url = '';
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  url = "http://localhost:3000/api/v1"
+} else {
+  url = "https://labonneauberge-server.herokuapp.com/api/v1"
+}
+
 
 export const find = async () => {
   const {data} = await axios.get(`${url}/events/1`);
