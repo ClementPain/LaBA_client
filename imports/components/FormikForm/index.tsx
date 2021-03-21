8da7f463a7f8364615ec2301ac2ccc0e984ea01a
clement <clementpain@outlook.com>
2021-03-21 14:56:14 +0100

    strange error
    Signed-off-by: clement <clementpain@outlook.com>

--- imports/components/FormikForm/index.tsx
+++ imports/components/FormikForm/index.tsx
@@ -4,14 +4,14 @@ import { Button, FormGroup } from 'react-bootstrap';
 import validate from './validate';
 
 interface FormikFormType {
-  initial_values: Object,
+  initialValues: Object,
   handleSubmit: (values:any) => void
 }
 
-const FormikForm: React.FC<FormikFormType> = ({ initial_values, handleSubmit, children }) => (
+const FormikForm: React.FC<FormikFormType> = ({ initialValues, handleSubmit, children }) => (
   <Formik
-    initialValues = { initial_values }
-    validate = { (values) => validate(values, initial_values) }
+    initialValues = { initialValues }
+    validate = { (values) => validate(values, initialValues) }
     onSubmit = { (values: Object, { setSubmitting }) => {
       setSubmitting(true);
       handleSubmit(values);
